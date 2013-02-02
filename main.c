9fbfede6ed4f513f227ac2018dd3174de037b60e
Carlos Scheidegger <cscheid@research.att.com>
2013-02-02 16:25:39 -0500

    final changes before first release

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+void example_1()
 {
     double *coordinates_out;
     int *tris_out;
@@ -26,5 +26,37 @@ int main()
     free(coordinates_out);
     if (tris_out)
         free(tris_out);
+}
+
+void example_2()
+{
+    double *coordinates_out;
+    int *tris_out;
+    int nverts, ntris, i;
+
+    double p[16] = { 0, 0, 3, 0, 3, 3, 0, 3, 1, 1, 2, 1, 2, 2, 1, 2 };
+    const double *contours[] = {p, p + 8, p+16};
+
+    tessellate(&coordinates_out, &nverts,
+               &tris_out, &ntris,
+               contours, contours + 3);
+
+    for (i=0; i<2 * nverts; ++i) {
+        fprintf(stdout, "%g ", coordinates_out[i]);
+    }
+    fprintf(stdout, "\n");
+    for (i=0; i<3 * ntris; ++i) {
+        fprintf(stdout, "%d ", tris_out[i]);
+    }
+    fprintf(stdout, "\n");
+    free(coordinates_out);
+    if (tris_out)
+        free(tris_out);
+}
+
+int main()
+{
+    example_1();
+    example_2();
     return 0;
 }
