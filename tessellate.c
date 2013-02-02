bb25f6a6bb170caefac550d9e9b0699a399f635b
Carlos Scheidegger <cscheid@research.att.com>
2013-02-02 14:18:08 -0500

    now reentrant

--- tessellate.c
+++ tessellate.c
@@ -5,154 +5,180 @@
 
 /******************************************************************************/
 
+typedef struct Triangle {
+    int v[3];
+    struct Triangle *prev;
+} Triangle;
+
 typedef struct Vertex {
     double pt[3];
     int index;
     struct Vertex *prev;
 } Vertex;
 
-Vertex *latest_v = NULL;
-Vertex *new_vertex(double x, double y)
+typedef struct TessContext {
+    Triangle *latest_t;
+    int n_tris;
+    
+    Vertex *v_prev;
+    Vertex *v_prevprev;
+    Vertex *latest_v;
+    GLenum current_mode;
+
+    void (*vertex_cb)(Vertex *, struct TessContext *);
+} TessContext;
+
+void skip_vertex(Vertex *v, TessContext *ctx);
+
+/******************************************************************************/
+
+TessContext *new_tess_context()
+{
+    TessContext *result = (TessContext *)malloc(sizeof (struct TessContext));
+    result->latest_t = NULL;
+    result->latest_v = NULL;
+    result->n_tris = 0;
+    result->v_prev = NULL;
+    result->v_prevprev = NULL;
+    result->v_prev = NULL;
+    result->v_prev = NULL;
+    result->vertex_cb = &skip_vertex;
+    return result;
+}
+
+void destroy_tess_context(TessContext *ctx)
+{
+    free(ctx);
+}
+
+Vertex *new_vertex(TessContext *ctx, double x, double y)
 {
     Vertex *result = (Vertex *)malloc(sizeof(Vertex));
-    result->prev = latest_v;
+    result->prev = ctx->latest_v;
     result->pt[0] = x;
     result->pt[1] = y;
     result->pt[2] = 0;
 
-    if (latest_v == NULL) {
+    if (ctx->latest_v == NULL) {
         result->index = 0;
     } else {
-        result->index = latest_v->index+1;
+        result->index = ctx->latest_v->index+1;
     }
-    return latest_v = result;
+    return ctx->latest_v = result;
 }
 
-typedef struct Triangle {
-    int v[3];
-    struct Triangle *prev;
-} Triangle;
 
-Triangle *latest_t = NULL;
-int n_tris = 0;
-Triangle *new_triangle(int v1, int v2, int v3)
+Triangle *new_triangle(TessContext *ctx, int v1, int v2, int v3)
 {
     Triangle *result = (Triangle *)malloc(sizeof(Triangle));
-    result->prev = latest_t;
+    result->prev = ctx->latest_t;
     result->v[0] = v1;
     result->v[1] = v2;
     result->v[2] = v3;
-    n_tris++;
-    return latest_t = result;
+    ctx->n_tris++;
+    return ctx->latest_t = result;
 }
 
 /******************************************************************************/
 
-Vertex *v_prev = NULL;
-Vertex *v_prevprev = NULL;
-GLenum current_mode;
+void skip_vertex(Vertex *v, TessContext *ctx) {};
 
-void (*vertex_cb)(Vertex *);
-
-void skip_vertex(Vertex *v) {};
-
-void fan_vertex(Vertex *v) {
-    if (v_prevprev == NULL) {
-        v_prevprev = v;
+void fan_vertex(Vertex *v, TessContext *ctx) {
+    if (ctx->v_prevprev == NULL) {
+        ctx->v_prevprev = v;
         return;
     }
-    if (v_prev == NULL) {
-        v_prev = v;
+    if (ctx->v_prev == NULL) {
+        ctx->v_prev = v;
         return;
     }
-    new_triangle(v_prevprev->index, v_prev->index, v->index);
-    v_prev = v;
+    new_triangle(ctx, ctx->v_prevprev->index, ctx->v_prev->index, v->index);
+    ctx->v_prev = v;
 }
 
-void strip_vertex(Vertex *v)
+void strip_vertex(Vertex *v, TessContext *ctx)
 { 
-    if (v_prevprev == NULL) {
-        v_prevprev = v;
+    if (ctx->v_prevprev == NULL) {
+        ctx->v_prevprev = v;
         return;
     }
-    if (v_prev == NULL) {
-        v_prev = v;
+    if (ctx->v_prev == NULL) {
+        ctx->v_prev = v;
         return;
     }
-    new_triangle(v_prevprev->index, v_prev->index, v->index);
+    new_triangle(ctx, ctx->v_prevprev->index, ctx->v_prev->index, v->index);
  
-    v_prev = v_prevprev;
-    v_prevprev = v;
+    ctx->v_prev = ctx->v_prevprev;
+    ctx->v_prevprev = v;
 }
 
-void triangle_vertex(Vertex *v) {
-    if (v_prevprev == NULL) {
-        v_prevprev = v;
+void triangle_vertex(Vertex *v, TessContext *ctx) {
+    if (ctx->v_prevprev == NULL) {
+        ctx->v_prevprev = v;
         return;
     }
-    if (v_prev == NULL) {
-        v_prev = v;
+    if (ctx->v_prev == NULL) {
+        ctx->v_prev = v;
         return;
     }
-    new_triangle(v_prevprev->index, v_prev->index, v->index);
-    v_prev = v_prevprev = NULL;
+    new_triangle(ctx, ctx->v_prevprev->index, ctx->v_prev->index, v->index);
+    ctx->v_prev = ctx->v_prevprev = NULL;
 }
 
-void vertex(void *vertex_data)
+void vertex(void *vertex_data, void *poly_data)
 {
     Vertex *ptr = (Vertex *)vertex_data;
-    vertex_cb(ptr);
+    TessContext *ctx = (TessContext *)poly_data;
+    ctx->vertex_cb(ptr, ctx);
 }
 
-void begin(GLenum which)
+void begin(GLenum which, void *poly_data)
 {
-    v_prev = v_prevprev = NULL;
+    TessContext *ctx = (TessContext *)poly_data;
+    ctx->v_prev = ctx->v_prevprev = NULL;
     switch (which) {
-    case GL_TRIANGLES: vertex_cb = &triangle_vertex; break;
-    case GL_TRIANGLE_STRIP: vertex_cb = &strip_vertex; break;
-    case GL_TRIANGLE_FAN: vertex_cb = &fan_vertex; break;
+    case GL_TRIANGLES: ctx->vertex_cb = &triangle_vertex; break;
+    case GL_TRIANGLE_STRIP: ctx->vertex_cb = &strip_vertex; break;
+    case GL_TRIANGLE_FAN: ctx->vertex_cb = &fan_vertex; break;
     default:
         fprintf(stderr, "ERROR, can't handle %d\n", (int)which);
-        vertex_cb = &skip_vertex;
+        ctx->vertex_cb = &skip_vertex;
     }
-}
-
-void end()
-{
 }
 
 void combine(const GLdouble newVertex[3],
              const void *neighborVertex[4],
-             const GLfloat neighborWeight[4], void **outData)
+             const GLfloat neighborWeight[4], void **outData, void *polyData)
 {
-    Vertex *result = new_vertex(newVertex[0], newVertex[1]);
+    TessContext *ctx = (TessContext *)polyData;
+    Vertex *result = new_vertex(ctx, newVertex[0], newVertex[1]);
     *outData = result;
 }
 
-void write_output(double **coordinates_out, int **tris_out, int *vc, int *tc)
+void write_output(TessContext *ctx, double **coordinates_out, int **tris_out, int *vc, int *tc)
 {
-    int n_verts = 1 + latest_v->index;
+    int n_verts = 1 + ctx->latest_v->index;
     *vc = n_verts;
-    int n_tris_copy = n_tris;
-    *tc = n_tris;
+    int n_tris_copy = ctx->n_tris;
+    *tc = ctx->n_tris;
     *coordinates_out = malloc(n_verts * sizeof(double) * 2);
-    *tris_out = (n_tris ? malloc(n_tris * sizeof(int) * 3) : NULL);
+    *tris_out = (ctx->n_tris ? malloc(ctx->n_tris * sizeof(int) * 3) : NULL);
 
-    while (latest_v) {
-        (*coordinates_out)[2*latest_v->index]   = latest_v->pt[0];
-        (*coordinates_out)[2*latest_v->index+1] = latest_v->pt[1];
-        Vertex *prev = latest_v->prev;
-        free(latest_v);
-        latest_v = prev;
+    while (ctx->latest_v) {
+        (*coordinates_out)[2*ctx->latest_v->index]   = ctx->latest_v->pt[0];
+        (*coordinates_out)[2*ctx->latest_v->index+1] = ctx->latest_v->pt[1];
+        Vertex *prev = ctx->latest_v->prev;
+        free(ctx->latest_v);
+        ctx->latest_v = prev;
     }
 
-    while (latest_t) {
-        (*tris_out)[3*(n_tris_copy-1)]   = latest_t->v[0];
-        (*tris_out)[3*(n_tris_copy-1)+1] = latest_t->v[1];
-        (*tris_out)[3*(n_tris_copy-1)+2] = latest_t->v[2];
-        Triangle *prev = latest_t->prev;
-        free(latest_t);
-        latest_t = prev;
+    while (ctx->latest_t) {
+        (*tris_out)[3*(n_tris_copy-1)]   = ctx->latest_t->v[0];
+        (*tris_out)[3*(n_tris_copy-1)+1] = ctx->latest_t->v[1];
+        (*tris_out)[3*(n_tris_copy-1)+2] = ctx->latest_t->v[2];
+        Triangle *prev = ctx->latest_t->prev;
+        free(ctx->latest_t);
+        ctx->latest_t = prev;
         n_tris_copy--;
     }
 }
@@ -168,21 +194,16 @@ void tessellate
     const double *contourbegin, *contourend;
     Vertex *current_vertex;
     GLUtesselator *tess;
+    TessContext *ctx;
 
-    latest_v = NULL;
-    latest_t = NULL;
-    v_prev = NULL;
-    v_prevprev = NULL;
-    n_tris = 0;
     tess = gluNewTess();
+    ctx = new_tess_context();
 
+    gluTessCallback(tess, GLU_TESS_VERTEX_DATA,  (GLvoid (*) ()) &vertex);
+    gluTessCallback(tess, GLU_TESS_BEGIN_DATA,   (GLvoid (*) ()) &begin);
+    gluTessCallback(tess, GLU_TESS_COMBINE_DATA, (GLvoid (*) ()) &combine);
 
-    gluTessCallback(tess, GLU_TESS_VERTEX,  (GLvoid (*) ()) &vertex);
-    gluTessCallback(tess, GLU_TESS_BEGIN,   (GLvoid (*) ()) &begin);
-    gluTessCallback(tess, GLU_TESS_END,     (GLvoid (*) ()) &end);
-    gluTessCallback(tess, GLU_TESS_COMBINE, (GLvoid (*) ()) &combine);
-
-    gluTessBeginPolygon(tess, 0);
+    gluTessBeginPolygon(tess, ctx);
     do {
         contourbegin = *contoursbegin++;
         if (contoursbegin == contoursend) {
@@ -191,7 +212,7 @@ void tessellate
         contourend = *contoursbegin;
         gluTessBeginContour(tess);
         while (contourbegin != contourend) {
-            current_vertex = new_vertex(contourbegin[0], contourbegin[1]);
+            current_vertex = new_vertex(ctx, contourbegin[0], contourbegin[1]);
             contourbegin += 2;
             gluTessVertex(tess, current_vertex->pt, current_vertex);
         }
@@ -199,7 +220,7 @@ void tessellate
     } while (1);
     gluTessEndPolygon(tess);
 
-    write_output(verts, tris, nverts, ntris);
-
+    write_output(ctx, verts, tris, nverts, ntris);
+    destroy_tess_context(ctx);
     gluDeleteTess(tess);
 }
---
+++ tessellate.js
@@ -0,0 +1,62 @@
+tessellate = (function() {
+
+var c_tessellate = Module.cwrap('tessellate', 'void', ['number', 'number', 'number', 
+                                                       'number', 'number', 'number']);
+var tessellate = function(vertices)
+{
+    debugger;
+    var i;
+    if (vertices.length % 2 !== 0)
+        throw "Expected even number of coordinates";
+
+    var ta = new Float64Array(vertices);
+
+    var p = Module._malloc(ta.byteLength);
+
+    for (i=0; i<ta.length; ++i)
+        Module.setValue(p+i*8, ta[i], "double");
+
+    var contours = Module._malloc(8);
+
+    Module.setValue(contours, p, 'i32');
+    Module.setValue(contours+4, p+ta.byteLength, 'i32');
+
+    var ppcoordinates_out = Module._malloc(4);
+    var pptris_out = Module._malloc(4);
+    var pnverts = Module._malloc(4);
+    var pntris = Module._malloc(4);
+
+    c_tessellate(ppcoordinates_out, pnverts, pptris_out, pntris, contours, contours+8);
+
+    var pcoordinates_out = Module.getValue(ppcoordinates_out, 'i32');
+    var ptris_out = Module.getValue(pptris_out, 'i32');
+
+    var nverts = Module.getValue(pnverts, 'i32');
+    var ntris = Module.getValue(pntris, 'i32');
+
+    var result_vertices = new Float64Array(nverts * 2);
+    var result_triangles = new Int32Array(ntris * 3);
+
+    for (i=0; i<2*nverts; ++i) {
+        result_vertices[i] = Module.getValue(pcoordinates_out + i*8, 'double');
+    }
+    for (i=0; i<3*ntris; ++i) {
+        result_triangles[i] = Module.getValue(ptris_out + i*4, 'i32');
+    }
+    Module._free(pnverts);
+    Module._free(pntris);
+    Module._free(ppcoordinates_out);
+    Module._free(pptris_out);
+    Module._free(pcoordinates_out);
+    Module._free(ptris_out);
+    Module._free(p);
+    Module._free(contours);
+    return {
+        vertices: result_vertices,
+        triangles: result_triangles
+    };
+};
+
+return tessellate;
+
+})();
